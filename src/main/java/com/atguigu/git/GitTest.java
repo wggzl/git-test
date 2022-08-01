9ea61a51b0351ba6ef418c4d8538435e1ea515b8
charles <727935022@qq.com>
2022-08-01 15:27:21 +0800

    冲突分支合并测试

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hot-fix test");
         System.out.println("github test");
         System.out.println("pull test");
+        System.out.println("master test");
     }
 }
