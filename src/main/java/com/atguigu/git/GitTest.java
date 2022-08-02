68b71c12a368d56d925faddc0fd535ba5b711e6f
charles <727935022@qq.com>
2022-08-02 16:03:28 +0800

    测试gitee的ssh功能

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("pull test");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("我是ssh");
     }
 }
